2cdbd351adca8e51880e85bb048b64127309d5b9
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2025-03-20 15:33:41 +0100

    example app for Spring Start chapter 11

--- src/main/java/com/example/sq_ch11_payments/config/ProjectConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.sq_ch11_payments.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "com.example.sq_ch11_payments.proxies")
-public class ProjectConfig {
-}
